c686a7bf74e792f934e36d5d028f5be003ae98d7
gammafp <gammafp@gmail.com>
2018-09-12 18:39:08 +0200

    Auto-generated commit

--- assets/vendor/libs.js
+++ assets/vendor/libs.js
@@ -1,4 +1,4 @@
-const $ = (x) => document.querySelector(x);
+const _$ = (x) => document.querySelector(x);
 
 // funcion creada por Lottie
 const clearString = string => {
@@ -19,14 +19,13 @@ const clearString = string => {
 const deleteSpriteInArray = (objectName, objectsSprite) => {
     return objectsSprite.filter(
         (x) =>
-            x.name != objectName
+        x.name != objectName
     );
 }
 
-// TODO: crear mejor herramienta
-const readMultipleFiles = function (files) {
-    const reader = new FileReader();
-    return new Promise((resolve, reject) => {
+const readMultipleFiles = (files) =>
+    new Promise((resolve, reject) => {
+        const reader = new FileReader();
         reader.onload = (x) => {
             const result = x.target.result;
             let img = new Image;
@@ -43,32 +42,59 @@ const readMultipleFiles = function (files) {
         }
         reader.readAsDataURL(files);
     });
-}
-
-// Reto
-
-// Magdiel
-const multiMAGDIEL = (arr, n, out = []) => {
-    if (arr.length < 1) return out;
-    out.push(arr.slice(0, n));
-    return multiMAGDIEL(arr.slice(n, arr.length), n, out);
-}
 
 // re
-const multiRE = (f, n) => (n === 0 ? n = 1 : n, f.length < 1 ? '' : [f.slice(0, n), ...multiRE(f.slice(n), n)]);
+const multiRE = (f, n) => (n === 0 ?
+    n = 1 :
+    n,
+    f.length < 1 ?
+    '' : [f.slice(0, n), ...multiRE(f.slice(n), n)]);
 
-// Multi lottie
-const multiLOTTIE = (obj, col) => {
-    if (col > obj.length) {
-        col = obj.length
+// Canvas
+class NacatamalON {
+
+    constructor() {}
+
+    addImage(imgSrc, x, y) {
+        // const img = new Image();
+        // img.src = imgSrc;
+
+        // this.canvas.width = img.width;
+        // this.canvas.height = img.height;
+
+        // this.ctx.clearRect(0, 0, img.width, img.height);
+        // this.ctx.drawImage(img, x, y);
     }
-
-    let m = []
-    let f = Math.ceil(obj.length / col);
-
-    for (i = 0; i < col; i++) {
-        m[i] = obj.splice(0, f)
+    getSizeImage(src) {
+        const img = new Image();
+        img.src = src;
+        return {
+            x: img.width,
+            y: img.height
+        }
     }
+    cutSpriteSheet(nameSpritesheet) {
+        // Ramda
+        return (src, grid, cutX, cutY) =>
+            grid.map((y, yi) =>
+                y.map((x, xi, x_array) => {
+                    const canvas = document.createElement('canvas');
+                    const ctx = canvas.getContext("2d");
+                    const img = new Image();
+                    img.src = src;
+                    canvas.width = cutX;
+                    canvas.height = cutY;
+                    ctx.clearRect(0, 0, canvas.width, canvas.height);
+                    ctx.drawImage(img, -(xi * cutX), -(yi * cutY));
+                    const output = {
+                        "name": `${nameSpritesheet}_${(x_array.length*yi+xi)}`,
+                        "result": canvas.toDataURL(),
+                        "width": cutX,
+                        "height": cutY
+                    };
+                    return output;
+                })
+            );
 
-    return m
+    }
 }
